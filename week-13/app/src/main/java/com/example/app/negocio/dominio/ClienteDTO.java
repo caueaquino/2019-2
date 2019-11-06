b7c55fac3a259415c7fccc150360649f1ee50dd5
Cauê Aquino Nogueira <caue@forlogic.net>
2019-11-06 20:27:02 -0300

    feat: added clienteDTO fixed

--- week-13/app/src/main/java/com/example/app/negocio/dominio/ClienteDTO.java
+++ week-13/app/src/main/java/com/example/app/negocio/dominio/ClienteDTO.java
@@ -4,7 +4,13 @@ import com.example.app.negocio.excecao.NomeMenorCincoCaracteresException;
 import com.example.app.negocio.excecao.PaisNaoDefinidoException;
 import com.example.app.negocio.validador.FabricaValidadorTelefone;
 import com.example.app.negocio.validador.TelefoneNaoCorrespondePaisException;
+import com.example.app.apresentacao.ClienteModel;
+import com.example.app.persistencia.Cliente;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -72,5 +78,67 @@ public class ClienteDTO {
         
         else
             throw new TelefoneNaoCorrespondePaisException();
+    }
+
+    public static ClienteDTO DTOFromEntity(Cliente cliente) {
+        return ClienteDTO.builder()
+                .id(cliente.getId())
+                .nome(cliente.getNome())
+                .idade(cliente.getIdade())
+                .telefone(cliente.getTefefone())
+                .limiteCredito(cliente.getLimiteCredito())
+                .pais(PaisDTO.valueOf(clinete.getPais()))
+                .build();
+    }
+
+    public static Set<ClienteDTO> DTOsFromEntities(List<Cliente> clientes) {
+        var resultado = new HashSet<PaisDTO>();
+
+        for (Cliente clienteAtual : clientes)
+            resultado.add(ClienteDTO.DTOFromEntity(clienteAtual));
+
+        return resultado;
+    }
+
+    public static Cliente EntityFromDTO (ClienteDTO cliente) {
+        return Cliente.builder()
+                .id(cliente.getId())
+                .nome(cliente.getNome())
+                .idade(cliente.getIdade())
+                .telefone(cliente.getTefefone())
+                .limiteCredito(cliente.getLimiteCredito())
+                .pais(PaisDTO.valueOf(clinete.getPais()))
+                .build();
+    }
+
+    public static Set<ClienteModel> ModelsFromDTOs (Set<ClienteDTO> clientes) {
+        var resultado = new HashSet<ClienteModel>();
+
+        for (ClienteDTO clienteAtual: clientes)
+            resultado.add(ClienteDTO.ModelFromDTO(clienteAtual));
+
+        return resultado;
+    }
+
+    public static ClienteModel ModelFromDTO (ClienteDTO cliente) {
+        return ClienteModel.builder()
+                .id(cliente.getId())
+                .nome(cliente.getNome())
+                .idade(cliente.getIdade())
+                .telefone(cliente.getTefefone())
+                .limiteCredito(cliente.getLimiteCredito())
+                .pais(PaisDTO.valueOf(clinete.getPais()))
+                .build();
+    }
+
+    public static ClienteDTO DTOFromModel (ClienteModel cliente) {
+        return ClienteDTO.builder()
+                .id(cliente.getId())
+                .nome(cliente.getNome())
+                .idade(cliente.getIdade())
+                .telefone(cliente.getTefefone())
+                .limiteCredito(cliente.getLimiteCredito())
+                .pais(PaisDTO.valueOf(clinete.getPais()))
+                .build();
     }
 }
